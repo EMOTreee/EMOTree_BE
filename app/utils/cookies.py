291786c35c816a748269268b35a2d42ed701aefb
jjjuni <0210leejun@gmail.com>
2025-11-24 00:19:54 +0900

    ✏️ Typo: 함수 이름 수정

--- app/utils/cookies.py
+++ app/utils/cookies.py
@@ -1,5 +1,5 @@
 from fastapi.responses import JSONResponse
 
-def clear_access_token_cookie(response: JSONResponse):
+def clear_token_cookie(response: JSONResponse):
     response.delete_cookie(key="access_token", path="/")
     return response
