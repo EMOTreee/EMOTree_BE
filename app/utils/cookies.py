86f12544c0223dee9d27435ae51e796bb4a304ad
jaeyeon <kasto6329@gmail.com>
2025-11-23 18:04:43 +0900

    ♻️ Refactor: access_token 쿠키 키 수정 및 set_token_cookie 제거

--- app/utils/cookies.py
+++ app/utils/cookies.py
@@ -1,16 +1,5 @@
 from fastapi.responses import JSONResponse
 
-def set_token_cookie(response: JSONResponse, token: str):
-    response.set_cookie(
-        key="kakao_token",
-        value=token,
-        httponly=True,
-        secure=False,
-        samesite="lax",
-        max_age=60*60*6
-    )
-    return response
-
-def clear_token_cookie(response: JSONResponse):
-    response.delete_cookie(key="kakao_token", path="/")
+def clear_access_token_cookie(response: JSONResponse):
+    response.delete_cookie(key="access_token", path="/")
     return response
