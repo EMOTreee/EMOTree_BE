bbba5a9901ce5e4cfa62830cc00051b803da415a
princehw03 <princehw03@gmail.com>
2025-12-08 02:37:56 +0900

    ✨ Feat: 감정공감 결과  db저장 기능 구현

--- app/routers/auth.py
+++ app/routers/auth.py
@@ -49,11 +49,34 @@ def logout():
     res = JSONResponse({"success": True})
     return clear_token_cookie(res)
 
-# 임시 테스트용 
-from app.utils.jwt_provider import *
-from fastapi import Response
-@router.post("/test-login")
-async def test_login(response: Response):
-    test_token = create_access_token({"user_id": 1})
-    response.set_cookie(key="access_token", value=test_token, httponly=True)
-    return {"message": "테스트 로그인 완료", "access_token": test_token}
+#---테스트용---------------------------------------------------------------------------------
+
+# 임시 테스트용 -> 더미유저 생성
+# from app.models.user import User  # User 모델 import
+# @router.post("/test-create-user")
+# def test_create_user(session: SessionDep):
+#     # 이미 존재하는 유저 있으면 삭제 (id=1 기준)
+#     existing_user = session.query(User).filter(User.id == 1).first()
+#     if existing_user:
+#         session.delete(existing_user)
+#         session.commit()
+
+#     dummy = User(
+#         kakao_id="dummy-kakao",
+#         nickname="테스트유저",
+#         profile_image=None
+#     )
+#     session.add(dummy)
+#     session.commit()
+#     session.refresh(dummy)
+
+#     return {"message": "dummy user created", "user": {"id": dummy.id, "nickname": dummy.nickname}}
+
+# # 임시 테스트용 -> 액세스토큰 발급
+# from app.utils.jwt_provider import *
+# from fastapi import Response
+# @router.post("/test-login")
+# async def test_login(response: Response):
+#     test_token = create_access_token({"user_id": 1})
+#     response.set_cookie(key="access_token", value=test_token, httponly=True)
+#     return {"message": "테스트 로그인 완료", "access_token": test_token}
